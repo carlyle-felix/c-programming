2a24b3c836a4536aae8f72ada531bac45acf9aa4
carlyle-felix <carlyle.felix@gmail.com>
2024-10-02 18:47:39 +0200

    shorten program

--- chapter 5/08.c
+++ chapter 5/08.c
@@ -27,47 +27,24 @@ int main(void){
 
     printf("Closest departure time is:  ");
 
-    if (minutes > 1400)
-        printf("null");
-    else if (minutes >= d1 && minutes < d2)
-        if (minutes - d1 < d2 - minutes)
-            printf("8:00 a.m., arriving at 10:16 a.m.\n");
-        else 
-            printf("9:43 a.m., arriving at 11:52 a.m.\n");
-    else if (minutes >= d2 && minutes < d3)
-        if (minutes - d2 < d3 - minutes)
-            printf("9:43 a.m., arriving at 11:52 a.m.\n");
-        else
-            printf("11:19 a.m., arriving at 1:31 p.m.\n");
-    else if (minutes >= d3 && minutes < d4)
-        if (minutes - d3 < d3 - minutes)
-            printf("11:19 a.m., arriving at 1:31 p.m.\n");
-        else 
-            printf("12:47 p.m., arriving at 3:00 p.m.\n");
-    else if (minutes >= d4 && minutes < d5)
-        if (minutes - d4 < d5 - minutes)
-            printf("12:47 p.m., arriving at 3:00 p.m.\n");
-        else 
-            printf("2:00 p.m., arriving at 4:08 p.m.\n");
-    else if (minutes >= d5 && minutes < d6)
-        if (minutes - d5 < d6 - minutes)
-            printf("2:00 p.m., arriving at 4:08 p.m.\n");
-        else 
-            printf("3:45 p.m., arriving at 5:55 p.m.\n");
-    else if (minutes >= d6 && minutes < d7)
-        if (minutes - d6 < d7 - minutes)
-            printf("3:45 p.m., arriving at 5:55 p.m.\n");
-        else 
-            printf("7:00 p.m., arriving at 9:20 p.m.\n");
-    else if (minutes >= d7 && minutes <= d8)
-        if (minutes - d7 < d8 - minutes)
-            printf("7:00 p.m., arriving at 9:20 p.m.\n");
-        else 
-            printf("9:45 p.m., arriving at 11:58 p.m.\n");
-    else if (minutes > d8_d1)
-            printf("8:00 a.m., arriving at 10:16 a.m.\n");
-    else
-            printf("9:45 p.m., arriving at 11:58 p.m.\n");
+    if (minutes < d8_d1)
+        printf("9:45 p.m., arriving at 11:58 p.m.\n");
+    else if (minutes <= (d1 + d2) / 2)
+        printf("8:00 a.m., arriving at 10:16 a.m.\n");
+    else if (minutes <= (d2 + d3) / 2)
+        printf("9:43 a.m., arriving at 11:52 a.m.\n");
+    else if (minutes <= (d3 + d4) / 2)
+        printf("11:19 a.m., arriving at 1:31 p.m.\n");
+    else if (minutes <= (d4 + d5) / 2)
+        printf("12:47 p.m., arriving at 3:00 p.m.\n");
+    else if (minutes <= (d5 + d6) / 2)
+        printf("2:00 p.m., arriving at 4:08 p.m.\n");
+    else if (minutes <= (d6 + d7) / 2)
+        printf("3:45 p.m., arriving at 5:55 p.m.\n");
+    else if (minutes <= (d7 + d8) / 2)
+        printf("7:00 p.m., arriving at 9:20 p.m.\n");
+    else    
+        printf("null.\n");
    
     return 0;
 }
