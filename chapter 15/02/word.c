73934e2be7f628db7f78707931112f0bdc273fad
carlyle-felix <carlyle.felix@gmail.com>
2024-11-27 09:24:17 +0200

    chapter 15 project 2

---
+++ chapter 15/02/word.c
@@ -0,0 +1,35 @@
+/* word.c (Chapter 15, page 363) */
+
+#include <stdio.h>
+#include "word.h"
+
+#define MAX_WORD_LEN 20
+
+int read_char(void)
+{
+    int ch = getchar();
+
+    if (ch == '\n' || ch == '\t')
+        return ' ';
+    return ch;
+}
+
+void read_word(char *word, int len)
+{
+    int ch, pos = 0;
+
+    while ((ch = read_char()) == ' ')
+        ;
+    while (ch != ' ' && ch != EOF)
+    {
+        if (pos < len)
+            word[pos++] = ch;
+        ch = read_char();
+    }
+
+    if (pos > MAX_WORD_LEN) {
+        word[MAX_WORD_LEN] = '*';
+        word[MAX_WORD_LEN+1] = '\0';
+    } else 
+        word[pos] = '\0';
+}
