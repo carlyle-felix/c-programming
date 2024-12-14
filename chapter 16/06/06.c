9443f3dc548124e6ba9082a78f49f13dcc50d993
carlyle-felix <carlyle.felix@gmail.com>
2024-12-14 17:45:44 +0200

    complete chapter 16

---
+++ chapter 16/06/06.c
@@ -0,0 +1,50 @@
+// compute which date comes earlier between 2 given dates.
+
+#include <stdio.h>
+
+struct date {
+    int day;
+    int month;
+    int year;
+};
+
+int compare_dates(struct date d1, struct date d2);
+
+int main(void){
+
+    struct date d1, d2;
+    int test;
+    printf("Enter first date (mm/dd/yy):    ");
+    scanf("%d/%d/%d", &d1.month, &d1.day, &d1.year);
+    printf("Enter second date (mm/dd/yy):    ");
+    scanf("%d/%d/%d", &d2.month, &d2.day, &d2.year);
+    
+    test = compare_dates(d1, d2);
+
+    if (test == 0) 
+        printf("Those are the same dates.\n");
+    else if (test > 0)
+        printf("%.2d/%.2d/%.2d is earlier than %.2d/%.2d/%.2d\n", d1.month, d1.day, d1.year, d2.month, d2.day, d2.year);
+    else    
+        printf("%.2d/%.2d/%.2d is earlier than %.2d/%.2d/%.2d\n", d2.month, d2.day, d2.year, d1.month, d1.day, d1.year);        
+    return 0;
+}
+
+int compare_dates(struct date d1, struct date d2) {
+    
+    if (d1.year < d2.year) {
+        return 1;
+    } else if (d1.month < d2.month) {
+        return 1;
+    } else if (d1.month < d2.month) {
+        return 1;
+    } else if (d1.year > d2.year) {
+        return -1;
+    } else if (d1.month > d2.month) {
+        return -1;
+    } else if (d1.day > d2.day) {
+        return -1;
+    } 
+
+    return 0;
+}
