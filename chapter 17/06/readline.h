16a2fee06a2c4a240c1b5c00ac0d5b11ca510aa0
carlyle-felix <carlyle.felix@gmail.com>
2025-01-15 21:00:02 +0200

    chapter 17 project 6

---
+++ chapter 17/06/readline.h
@@ -0,0 +1,6 @@
+#ifndef READLINE_H
+#define READLINE_h
+
+int read_line(char str[], int n);
+
+#endif
---
+++ chapter 17/06/sort.c
@@ -0,0 +1,54 @@
+// Sort s series of words entered by the user
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "readline.h"
+
+#define MAX_WORDS 50
+#define MAX_WORD_LEN 20
+
+int compare(const void *p1, const void *p2);
+
+int main(void) {
+
+    char *word[MAX_WORDS];
+    int i, count = 1;
+
+    for (i = 0; i < MAX_WORDS; i++) {
+       
+
+        word[i] = malloc(MAX_WORD_LEN + 1);
+        if (word[i] == NULL) {
+            printf("Error: malloc failed to allocate initial space for word[%d].\n", i + 1);
+            exit(EXIT_FAILURE);
+        }
+        printf("Enter word: ");
+        
+        count = read_line(word[i], MAX_WORD_LEN);
+        if (count == 0) {
+            free(word[i]);
+            break;
+        }
+
+        word[i] = realloc(word[i], count);
+        if (word[i] == NULL) {
+            printf("Error: realloc failed to resize word[%d].\n", i + 1);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    qsort(word, i, sizeof(char *), compare);
+
+    printf("In sorted order: ");
+    for (count = i, i = 0; i < count; i++) {
+        printf("%s ", word[i]);
+    }
+    putchar('\n');
+
+    return 0;
+}
+
+int compare(const void *p1, const void *p2) {
+
+    return strcmp(*(char **)p1, *(char **)p2);
+}
